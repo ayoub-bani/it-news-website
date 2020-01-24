03c1b7c2b7a297d13e0fd236a1f5739234703725
ayoub-bani <ayoub.bani101@gmail.com>
2020-01-24 22:27:35 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './component/layout/nav-bar/nav-bar.component';
+import { FooterComponent } from './component/layout/footer/footer.component';
+import { SliderComponent } from './component/slider/slider.component';
+import { SideNewsComponent } from './component/layout/side-news/side-news.component';
+import { BodyArticlesComponent } from './component/body-articles/body-articles.component';
+import { FooterArticlesComponent } from './component/footer-articles/footer-articles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    SliderComponent,
+    SideNewsComponent,
+    BodyArticlesComponent,
+    FooterArticlesComponent
   ],
   imports: [
     BrowserModule,
