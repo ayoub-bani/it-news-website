cf7cb6c588edf40021f75b280e91f564e2de2549
ayoub-bani <ayoub.bani101@gmail.com>
2020-01-29 14:40:09 +0100

    unitial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NationalComponent } from './national/national.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { WorldComponent } from './world/world.component';
+import { SportsComponent } from './sports/sports.component';
+import { BusinessComponent } from './business/business.component';
+import { WeatherComponent } from './weather/weather.component';
+import { OpinionComponent } from './opinion/opinion.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomePageComponent },
+  { path: "national", component: NationalComponent },
+  { path: "world", component: WorldComponent },
+  { path: "sports", component: SportsComponent },
+  { path: "business", component: BusinessComponent },
+  { path: "weather", component: WeatherComponent },
+  { path: "dashboard", component: DashboardComponent },
+  // { path: "calculator", component: CalculatorComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,19 @@ import { SliderComponent } from './component/slider/slider.component';
 import { SideNewsComponent } from './component/layout/side-news/side-news.component';
 import { BodyArticlesComponent } from './component/body-articles/body-articles.component';
 import { FooterArticlesComponent } from './component/footer-articles/footer-articles.component';
+import { PagesComponent } from './pages/pages.component';
+import { NationalComponent } from './national/national.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { WorldComponent } from './world/world.component';
+import { SportsComponent } from './sports/sports.component';
+import { BusinessComponent } from './business/business.component';
+import { WeatherComponent } from './weather/weather.component';
+import { OpinionComponent } from './opinion/opinion.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LeftsidebarComponent } from './dashboard/leftsidebar/leftsidebar.component';
+import { MainpageComponent } from './dashboard/mainpage/mainpage.component';
+import { RightsidebarComponent } from './dashboard/rightsidebar/rightsidebar.component';
+import { TopcardsComponent } from './dashboard/topcards/topcards.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +31,20 @@ import { FooterArticlesComponent } from './component/footer-articles/footer-arti
     SliderComponent,
     SideNewsComponent,
     BodyArticlesComponent,
-    FooterArticlesComponent
+    FooterArticlesComponent,
+    PagesComponent,
+    NationalComponent,
+    HomePageComponent,
+    WorldComponent,
+    SportsComponent,
+    BusinessComponent,
+    WeatherComponent,
+    OpinionComponent,
+    DashboardComponent,
+    LeftsidebarComponent,
+    MainpageComponent,
+    RightsidebarComponent,
+    TopcardsComponent
   ],
   imports: [
     BrowserModule,
